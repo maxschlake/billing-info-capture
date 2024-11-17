441d0d5c9931aaca7eca94ebc3afc25e061663eb
maxschlake <98614848+maxschlake@users.noreply.github.com>
2024-11-17 17:20:13 +0100

    Typo in main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ import json
 # Demonstration with two example invoices:
 invoiceList = []
 
-# Run each invoice throug the pipeline and append the captured data to the invoice list
+# Run each invoice through the pipeline and append the captured data to the invoice list
 for invoice in [["facture1", "jpg"], ["facture2", "jpg"]]:
 
     capturedData = createDataDict(invoice[0], invoice[1], nerModel="fr_core_news_lg", saveImages=True)
     invoiceList.append(capturedData)
 
-# Write athe list of invoices into a JSON file
+# Write the list of invoices into a JSON file
 with open("invoiceData.json", "w") as jsonFile:
     json.dump(invoiceList, jsonFile, indent=4)
